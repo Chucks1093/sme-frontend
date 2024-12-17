5ad2e2dd883dece0039b47987c185cb642d03e94
Anioke sebastian <aniokechukwudi7@gmail.com>
2024-12-17 13:34:11 +0100

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [nodePolyfills(), react()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
